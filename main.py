dd53862f5f713b2b6e1c98265602698223716eeb
naqibasri <naqib.asri.294@gmail.com>
2023-12-30 13:43:50 +0800

    main.py v1 finished

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 import cv2 as cv
+import os 
 
 cap = cv.VideoCapture(0)
-cap.set(3,1280)
-cap.set(4,720)
+cap.set(3,640)
+cap.set(4,480)
+
+imgBackground = cv.imread('Resources/background.png')
+
+folderModePath = 'Resources/Modes'
+modePathList = os.listdir(folderModePath)
+imgModeList = []
+for path in modePathList:
+    imgModeList.append(cv.imread(os.path.join(folderModePath,path)))
 
 while True:
     success, img = cap.read()
-    cv.imshow('Face Atendence', img)
-    cv.waitKey(1)
+
+    imgBackground[162:162+480,55:55+640] = img
+    imgBackground[44:44+633, 808:808+414] = imgModeList[0]
+
+    #cv.imshow('Webcam', img)
+    cv.imshow('Face Attendence', imgBackground)
+    cv.waitKey(1) 
